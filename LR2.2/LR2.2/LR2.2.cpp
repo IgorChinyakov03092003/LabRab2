7aef665e564b07d56d80b593ea8cc9326bbf459d
HoobahBoobah <igor.chinyakov.03@mail.ru>
2023-02-20 10:04:12 +0300

    Add files via upload

---
+++ LR2.2/LR2.2/LR2.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    std::stack<char> s;
+    char c;
+    for (char c; std::cin >> c;) {
+        switch (c) {
+        case '(': case '{': case '[':
+            s.push(c);
+            break;
+        case ')': case '}': case ']':
+            if (s.empty() || (c==')' && s.top() != '(') || (c == '}' && s.top() != '{') || (c==']' && s.top() != '[')) {
+                std::cout << "no";
+                return 0;
+            }
+            s.pop();
+            break;
+        }
+    }
+    std::cout << (s.empty() ? "yes" : "no");
+    return 0;
+}
+
